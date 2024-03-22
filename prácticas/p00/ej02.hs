a4e0f0d7b995e7174b12502a8ff68e8f84135f64
Joni Bekenstein <joni@bek.io>
2024-03-22 14:37:45 -0300

    Optimiza cantDivisoresPrimos usando la criba

--- prácticas/p00/ej02.hs
+++ prácticas/p00/ej02.hs
@@ -13,18 +13,11 @@ factorial 0 = 1
 factorial n = n * factorial (n-1)
 
 cantDivisoresPrimos :: Int -> Int
-cantDivisoresPrimos n = foldl (+) 0 (map esDivisible (filter esPrimo [1..n]))
-    where esDivisible = fromEnum . (== 0) . (n `mod`)
+cantDivisoresPrimos n = sum (map esDivisible primos)
+    where
+        primos = criba [2..(floor (sqrt (fromIntegral n)))]
+        esDivisible = fromEnum . (== 0) . (n `mod`)
 
-esPrimo :: Int -> Bool
-esPrimo 1 = False
-esPrimo n = cantDivisores n == 2
-
-cantDivisores :: Int -> Int
-cantDivisores n = cantDivisoresDesde n 1
-
-cantDivisoresDesde :: Int -> Int -> Int
-cantDivisoresDesde n m
-    | m > n = 0
-    | n `mod` m == 0 = 1 + cantDivisoresDesde n (m+1)
-    | otherwise = cantDivisoresDesde n (m+1)
+criba :: [Int] -> [Int]
+criba [] = []
+criba (x:xs) = x : criba (filter ((/= 0) . (`mod` x)) xs)
