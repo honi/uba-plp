5a4a7a284dc6c387fb9c203a11ec871ddd12d5c7
Joni Bekenstein <joni@bek.io>
2024-03-26 12:45:37 -0300

    Completa ejercicio 4

--- prácticas/p01/ej04.hs
+++ prácticas/p01/ej04.hs
@@ -1,8 +1,24 @@
 permutaciones :: [a] -> [[a]]
 permutaciones =
-    foldr (\x r ->
+    foldr (\x rs ->
         concatMap (\r ->
             map (insert x r) [0..length r]
-        ) r
+        ) rs
     ) [[]]
     where insert x r i = drop i r ++ [x] ++ take i r
+
+-- Preguntar
+partes :: [a] -> [[a]]
+partes = foldl (\rs x -> rs ++ map (\r -> r ++ [x]) rs) [[]]
+
+prefijos :: [a] -> [[a]]
+prefijos = foldl (\rs x -> rs ++ [last rs ++ [x]]) [[]]
+
+sublistas :: [a] -> [[a]]
+-- sublistas [] = [[]]
+-- sublistas (x:xs) = map (x :) (prefijos xs) ++ sublistas xs
+sublistas = recr (\x xs r -> map (x :) (prefijos xs) ++ r) [[]]
+
+recr :: (a -> [a] -> b -> b) -> b -> [a] -> b
+recr f z [] = z
+recr f z (x : xs) = f x xs (recr f z xs)
