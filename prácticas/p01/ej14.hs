0e2e4afe3bdb80018499f7235729c9d3ff293afc
Joni Bekenstein <joni@bek.io>
2024-05-21 15:15:38 -0300

    Agrega función mismaEstructura

--- prácticas/p01/ej14.hs
+++ prácticas/p01/ej14.hs
@@ -16,3 +16,10 @@ cantHojas = foldAB 0 (\rl v rr -> if rl == 0 && rr == 0 then 1 else 0 + rl + rr)
 
 espejo :: AB a -> AB a
 espejo = foldAB Nil (\rl v rr -> Bin rr v rl)
+
+mismaEstructura :: AB a -> AB b -> Bool
+mismaEstructura = recAB esNil f
+    where
+        f xl _ xr xrl xrr y = case y of
+            Nil -> False -- Bin(...) != Nil
+            (Bin yl _ yr) -> esNil xl == esNil yl && esNil xr == esNil yr && xrl yl && xrr yr
