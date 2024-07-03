11fc370c41eda414b69fb5a800eee2c628912ac1
Joni Bekenstein <joni@bek.io>
2024-07-03 10:44:20 -0300

    Resuelve iésimo de forma más declarativa

--- prácticas/p08/ej07.pl
+++ prácticas/p08/ej07.pl
@@ -2,5 +2,8 @@
 palíndromo(L, L1) :- reverse(L, R), append(L, R, L1).
 
 % iésimo(?I, +L, -X), donde X es el I-ésimo elemento de la lista L.
-iésimo(1, [X|_], X).
-iésimo(I, [_|L], X) :- iésimo(J, L, X), I is J+1.
+iésimo(I, L, X) :- append(P, [X|_], L), length(P, N), I is N+1.
+
+% Versión recursiva.
+% iésimo(1, [X|_], X).
+% iésimo(I, [_|L], X) :- iésimo(J, L, X), I is J+1.
